dde3b8583986028f8052c58d1b9a1e9595183e96
ltaytson <leotaytson@gmail.com>
2020-05-11 11:16:33 -0300

    show environment

--- public/index.php
+++ public/index.php
@@ -1,8 +1,12 @@
+<?php
+    include 'config.php';
+?>
+
 <html>
  <head>
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo "<p>Hello World {$_SERVER['SERVER_ADDR']} </p>" ?> 
+ <?php echo "<p>Hello World {$_SERVER['SERVER_ADDR']} - {$env}</p>" ?> 
  </body>
 </html>
